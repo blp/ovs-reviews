08d12842bba0a8474261cfa0e33d3b8181e41755
Ben Pfaff <blp@nicira.com>
2015-05-17 13:10:02 -0700

    vlog: Remove unneeded #include.
    Signed-off-by: Ben Pfaff <blp@nicira.com>

--- lib/vlog.c
+++ lib/vlog.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008, 2009, 2010, 2011, 2012, 2013 Nicira, Inc.
+ * Copyright (c) 2008, 2009, 2010, 2011, 2012, 2013, 2015 Nicira, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -34,7 +34,6 @@
 #include "dynamic-string.h"
 #include "ofpbuf.h"
 #include "ovs-thread.h"
-#include "sat-math.h"
 #include "socket-util.h"
 #include "svec.h"
 #include "syslog-direct.h"
