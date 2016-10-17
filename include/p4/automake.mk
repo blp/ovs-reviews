41508cca608e15ad7dc2ce74c65695a4bf54234b
Ben Pfaff <blp@ovn.org>
2016-10-17 11:06:15 -0700

    Remove include/p4/src/README.md, which is no longer relevant.
    Signed-off-by: Ben Pfaff <blp@ovn.org>

--- include/p4/automake.mk
+++ include/p4/automake.mk
@@ -2,5 +2,4 @@ EXTRA_DIST += \
 	include/p4/examples/l2-switch.p4 \
 	include/p4/examples/simple-router.p4 \
 	include/p4/header.p4c \
-	include/p4/plugin/helpers.py \
-	include/p4/src/README.md
+	include/p4/plugin/helpers.py
