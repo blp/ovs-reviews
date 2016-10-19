6d538a05dc5529fe239733f268fbb535e137b8ff
Ben Pfaff <blp@ovn.org>
2016-10-19 13:29:46 -0700

    ovn-northd: Use synthetic column for L2 port security.
    Signed-off-by: Ben Pfaff <blp@ovn.org>

---
+++ ovn/northd/ovn-northd.h
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2016 Nicira, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef OVN_NORTHD_H
+#define OVN_NORTHD_H 1
+
+#include <stddef.h>
+
+struct lport_addresses;
+void build_ps_addrs(char **port_security, size_t n_port_security,
+                    struct lport_addresses **ps_addrsp, size_t *n_ps_addrsp);
+void free_ps_addrs(struct lport_addresses *, size_t n);
+
+char *build_port_security_l2(const char *port_field, const char *port,
+                             const char *eth_addr_field,
+                             struct lport_addresses *, size_t n);
+
+char *build_port_security_nd(const char *port,
+                             struct lport_addresses *, size_t n);
+
+#endif /* ovn-northd.h */
