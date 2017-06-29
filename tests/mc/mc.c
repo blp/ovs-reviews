1e1919a8ca2ec7f5f2ed72f85d5ef2eb3cb67982
Syed Akbar Mehdi <mehdis@vmware.com>
2017-06-28 18:23:52 -0700

    Started adding code to model checker. Reading configuration from json

--- tests/mc/mc.c
+++ tests/mc/mc.c
@@ -15,20 +15,40 @@
  */
 
 #include <config.h>
-#include "mc.h"
-#include "openvswitch/vlog.h"
 #include "jsonrpc.h"
+#include "mc.h"
+#include "openvswitch/dynamic-string.h"
+#include "openvswitch/json.h"
+#include "openvswitch/list.h"
+#include "openvswitch/vlog.h"
+#include "process.h"
+#include "util.h"
 
 VLOG_DEFINE_THIS_MODULE(mc);
 
-struct mc_conn {
+struct mc_process {
     struct ovs_list list_node;
     struct jsonrpc_session *js;
+    char* name;
     struct uuid sid;
 };
 
+static struct ovs_list
+    
 int
 main(int argc, char *argv[])
 {
+    if (argc < 2) {
+	ovs_fatal(0, "Not enough arguments provided to raft-client");
+    }
+
+    struct json *config = json_from_file(argv[1]);
+
+    if (config->type == JSON_STRING) {
+	ovs_fatal(0, "Cannot read the json config in %s\n%s", argv[1], config->u.string);
+    }
+
+    
+    
     return 0;
 }
