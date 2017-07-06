b1b2ad76d9362f8b962f79b37e477600875d86ef
Syed Akbar Mehdi <mehdis@vmware.com>
2017-07-05 19:07:41 -0700

    Changed interface of mc<->client rpcs

--- tests/mc/mc.h
+++ tests/mc/mc.h
@@ -19,43 +19,58 @@
 
 #include "uuid.h"
 
-
-enum mc_rpc_client_msg_type {
-    MC_RPC_CHOOSE,
+enum mc_rpc_msg_type {
+    MC_RPC_HELLO,
+    MC_RPC_CHOOSE_REQ,
+    MC_RPC_CHOOSE_REPLY,
     MC_RPC_ASSERT
 };
 
 enum mc_rpc_choose_req_type {
-    MC_RPC_CHOOSE_LOG_OPEN,
-    MC_RPC_CHOOSE_LOG_READ,
-    MC_RPC_CHOOSE_LOG_WRITE,
-    MC_RPC_CHOOSE_LOG_COMMIT,
-    MC_RPC_CHOOSE_LOG_REPLACE_START,
-    MC_RPC_CHOOSE_LOG_REPLACE_COMMIT,
-    MC_RPC_CHOOSE_TIMER,
-    MC_RPC_CHOOSE_NETWORK_OPEN,
-    MC_RPC_CHOOSE_NETWORK_SEND,
-    MC_RPC_CHOOSE_NETWORK_RECV,
-    MC_RPC_NONE
+    MC_RPC_CHOOSE_REQ_LOG,
+    MC_RPC_CHOOSE_REQ_TIMER,
+    MC_RPC_CHOOSE_REQ_NETWORK,
 };
 
-struct mc_rpc_client_msg_common {
-    enum mc_rpc_client_msg_type type;
-    struct uuid sid;
+enum mc_rpc_choose_subtype {
+    MC_RPC_SUBTYPE_OPEN,
+    MC_RPC_SUBTYPE_READ,
+    MC_RPC_SUBTYPE_WRITE,
+    MC_RPC_SUBTYPE_SEND,
+    MC_RPC_SUBTYPE_RECV,
+    MC_RPC_SUBTYPE_COMMIT,
+    MC_RPC_SUBTYPE_REPLACE_START,
+    MC_RPC_SUBTYPE_REPLACE_COMMIT
+};
+
+enum mc_rpc_choose_reply_type {
+    MC_RPC_CHOOSE_REPLY_NORMAL,
+    MC_RPC_CHOOSE_REPLY_ERROR,
+};
+
+struct mc_rpc_common {
+    enum mc_rpc_msg_type type;
+    /* Due to the single machine setup
+     * pid is a good way to identify the 
+     * sender given that the model checker 
+     * spawns all processes */
+    pid_t pid;
+};
+
+struct mc_rpc_hello {
+    struct mc_rpc_common common;
 };
 
 struct mc_rpc_choose_req {
-    struct mc_rpc_client_msg_common common;
+    struct mc_rpc_common common;
     enum mc_rpc_choose_req_type type;
-};
-
-enum mc_rpc_server_msg_type {
-    MC_RPC_REPLY_NORMAL,
-    MC_RPC_REPLY_ERROR
+    enum mc_rpc_choose_subtype subtype;
+    void* data;
 };
 
 struct mc_rpc_choose_reply {
-    enum mc_rpc_server_msg_type type;
+    struct mc_rpc_common common;
+    enum mc_rpc_choose_reply_type reply;
 };
 
 #endif /* tests/mc/mc.h */
