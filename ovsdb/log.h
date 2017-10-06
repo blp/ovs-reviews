a14284f69260b30407c3cb939fbc1e508db7a8c8
Ben Pfaff <blp@ovn.org>
2017-10-06 11:39:10 -0700

    log: New function ovsdb_log_compose_record().
    Signed-off-by: Ben Pfaff <blp@ovn.org>

--- ovsdb/log.h
+++ ovsdb/log.h
@@ -19,6 +19,7 @@
 #include <sys/types.h>
 #include "compiler.h"
 
+struct ds;
 struct json;
 struct ovsdb_log;
 
@@ -41,6 +42,9 @@ void ovsdb_log_close(struct ovsdb_log *);
 struct ovsdb_error *ovsdb_log_read(struct ovsdb_log *, struct json **)
     OVS_WARN_UNUSED_RESULT;
 void ovsdb_log_unread(struct ovsdb_log *);
+
+void ovsdb_log_compose_record(const struct json *, const char *magic,
+                              struct ds *header, struct ds *data);
 
 struct ovsdb_error *ovsdb_log_write(struct ovsdb_log *, const struct json *)
     OVS_WARN_UNUSED_RESULT;
