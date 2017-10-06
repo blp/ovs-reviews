e4d44c08202fae67df39f04d5fa9357fd5696c26
Ben Pfaff <blp@ovn.org>
2017-10-06 11:37:42 -0700

    ovsdb-client: Add new "backup" command.
    Signed-off-by: Ben Pfaff <blp@ovn.org>

--- ovsdb/file.c
+++ ovsdb/file.c
@@ -566,6 +566,19 @@ ovsdb_file_txn_to_json(const struct ovsdb_txn *txn)
     return ftxn.json;
 }
 
+struct json *
+ovsdb_file_txn_annotate(struct json *json, const char *comment)
+{
+    if (!json) {
+        json = json_object_create();
+    }
+    if (comment) {
+        json_object_put_string(json, "_comment", comment);
+    }
+    json_object_put(json, "_date", json_integer_create(time_wall_msec()));
+    return json;
+}
+
 struct ovsdb_error *
 ovsdb_file_commit(struct ovsdb_file *file,
                   const struct ovsdb_txn *txn, bool durable)
@@ -824,14 +837,7 @@ ovsdb_file_txn_commit(struct json *json, const char *comment,
 {
     struct ovsdb_error *error;
 
-    if (!json) {
-        json = json_object_create();
-    }
-    if (comment) {
-        json_object_put_string(json, "_comment", comment);
-    }
-    json_object_put(json, "_date", json_integer_create(time_wall_msec()));
-
+    json = ovsdb_file_txn_annotate(json, comment);
     error = ovsdb_log_write(log, json);
     json_destroy(json);
     if (error) {
