2cee90eb6f50c137e6ad2d0e057604b63ebd6d84
Ben Pfaff <blp@ovn.org>
2020-02-11 14:57:37 -0800

    northd: Split up automake.mk to improve ovsdb2ddlog dependencies.
    With all of the configuration for generating the DDlog files for OVSDB
    schemas in the makefile itself, there's not a graceful way to
    regenerate the DDlog when the configuration changes (e.g. because some
    new field or table needs to be handled) because it's too expensive to
    regenerate them whenever the Makefile changes at all.  But by putting
    the configuration in a separate script, we can add a more granular
    dependency.
    
    Signed-off-by: Ben Pfaff <blp@ovn.org>

--- northd/automake.mk
+++ northd/automake.mk
@@ -15,7 +15,8 @@ EXTRA_DIST += \
 	northd/ovn.toml northd/lswitch.dl northd/lrouter.dl \
 	northd/helpers.dl northd/ipam.dl northd/multicast.dl \
 	northd/docs/design.md  northd/docs/debugging.md \
-	northd/docs/new-feature-tutorial.md
+	northd/docs/new-feature-tutorial.md \
+	northd/nb2ddlog northd/sb2ddlog
 
 if DDLOG
 BUILT_SOURCES += northd/ovn_northd_ddlog/ddlog.h
@@ -30,64 +31,13 @@ northd_ovn_northd_ddlog_LDADD = \
 	$(OVS_LIBDIR)/libopenvswitch.la \
 	northd/ovn_northd_ddlog/target/release/libovn_northd_ddlog.la
 
-northd/OVN_Northbound.dl: ovn-nb.ovsschema
-	ovsdb2ddlog -f $(srcdir)/ovn-nb.ovsschema         \
-				-o Logical_Switch_Port          \
-				-k Logical_Switch_Port.name     \
-				-o NB_Global                    \
-				--ro NB_Global.name             \
-				--ro NB_Global.nb_cfg           \
-				--ro NB_Global.external_ids     \
-				--ro NB_Global.connections      \
-				--ro NB_Global.ssl              \
-				> $@.tmp && mv $@.tmp $@
+northd/OVN_Northbound.dl: ovn-nb.ovsschema northd/nb2ddlog
+	$(AM_V_GEN)$(srcdir)/northd/nb2ddlog $(srcdir)/ovn-nb.ovsschema > $@.tmp
+	$(AM_V_at)mv $@.tmp $@
 
-northd/OVN_Southbound.dl: ovn-sb.ovsschema
-	ovsdb2ddlog -f $(srcdir)/ovn-sb.ovsschema \
-				-o SB_Global        	\
-				-o Logical_Flow     	\
-				-o Multicast_Group  	\
-				-o Meter            	\
-				-o Meter_Band       	\
-				-o Datapath_Binding 	\
-				-o Port_Binding     	\
-				-o Gateway_Chassis  	\
-				-o Port_Group       	\
-				-o MAC_Binding      	\
-				-o DHCP_Options     	\
-				-o DHCPv6_Options   	\
-				-o Address_Set      	\
-				-o DNS              	\
-				-o RBAC_Role        	\
-				-o RBAC_Permission  	\
-				-o IP_Multicast         \
-				-p Datapath_Binding 	\
-				-p Port_Binding     	\
-				-p Multicast_Group      \
-				--ro Port_Binding.chassis       \
-				--ro Port_Binding.encap         \
-				--ro IP_Multicast.seq_no        \
-				--ro SB_Global.ssl              \
-				--ro SB_Global.connections      \
-				--ro SB_Global.external_ids     \
-				-k Multicast_Group.datapath     \
-				-k Multicast_Group.name         \
-				-k Multicast_Group.tunnel_key   \
-				-k Port_Binding.logical_port    \
-				-k DNS.external_ids             \
-				-k Datapath_Binding.external_ids\
-				-k RBAC_Role.name               \
-				-k Address_Set.name             \
-				-k Port_Group.name              \
-				-k Meter.name                   \
-				-k Logical_Flow.logical_datapath\
-				-k Logical_Flow.pipeline		\
-				-k Logical_Flow.table_id		\
-				-k Logical_Flow.priority		\
-				-k Logical_Flow.match			\
-				-k Logical_Flow.actions			\
-				-k IP_Multicast.datapath		\
-				> $@.tmp && mv $@.tmp $@
+northd/OVN_Southbound.dl: ovn-sb.ovsschema northd/sb2ddlog
+	$(AM_V_GEN)$(srcdir)/northd/sb2ddlog $(srcdir)/ovn-sb.ovsschema > $@.tmp
+	$(AM_V_at)mv $@.tmp $@
 
 CLEANFILES += northd/OVN_Northbound.dl northd/OVN_Southbound.dl
 
