blp/ovs-reviews

commit 90ab4b5af497fbe3dc4276ede44a06930089a874

northd: Fix up controller event support.
This was supported before, but I screwed up and added redundant
support.  This commit removes the duplication and properly adds support
for options:controller-event, which had been overlooked.

Signed-off-by: Ben Pfaff <blp@ovn.org>

northd/ovn_northd.dl
@@ -1471,7 +1471,7 @@ HasEventElbMeter(true) :-
     nb.Meter(.name = "event-elb").
 
 HasEventElbMeter(false) :-
-    nb.NB_Global(._uuid = uuid),
+    Unit(),
     not nb.Meter(.name = "event-elb").
 
 /* Empty LoadBalancer Controller event */
@@ -1514,20 +1514,25 @@ function build_empty_lb_event_flow(key: string, lb: nb.Load_Balancer,
     Some{(__match, __action)}
 }
 
-for (SwitchLBVIP(.sw = &sw, .lb = &lb, .vip = (vip_key, vip_val))) {
-    if (vip_val == "") {
-            for (HasEventElbMeter(has_elb_meter)) {
-                Some {(var __match, var __action)} =
-                    build_empty_lb_event_flow(vip_key, lb, has_elb_meter) in
-                    Flow(.logical_datapath = sw.dpname,
-                         .stage            = switch_stage(IN, PRE_LB),
-                         .priority         = 130,
-                         .__match          = __match,
-                         .actions          = __action,
-                         .external_ids     = map_empty())
-            }
-    }
-}
+/* ControllerEventEn has exactly one row, either 'true' to enable controller
+ * events or 'false' to disable them. */
+relation ControllerEventEn(enable: bool)
+ControllerEventEn(map_get_bool_def(options, "controller-event", false)) :-
+    nb.NB_Global(.options = options).
+ControllerEventEn(false) :- Unit(), not nb.NB_Global().
+
+Flow(.logical_datapath = sw.dpname,
+     .stage            = switch_stage(IN, PRE_LB),
+     .priority         = 130,
+     .__match          = __match,
+     .actions          = __action,
+     .external_ids     = map_empty()) :-
+    ControllerEventEn(true),
+    SwitchLBVIP(.sw = &sw, .lb = &lb, .vip = (vip_key, vip_val)),
+    vip_val == "",
+    HasEventElbMeter(has_elb_meter),
+    Some {(var __match, var __action)} = build_empty_lb_event_flow(
+        vip_key, lb, has_elb_meter).
 
 /* 'REGBIT_CONNTRACK_DEFRAG' is set to let the pre-stateful table send
  * packet to conntrack for defragmentation. */
@@ -1555,29 +1560,6 @@ for (SwitchLBVIP(.sw = &sw, .vip = vip)) {
          .actions          = "${rEGBIT_CONNTRACK_DEFRAG()} = 1; next;",
          .external_ids     = map_empty())
 }
-
-/* ControllerEventEn has exactly one row, either 'true' to enable controller
- * events or 'false' to disable them. */
-relation ControllerEventEn(enable: bool)
-ControllerEventEn(map_get_bool_def(options, "controller-event", false)) :-
-    nb.NB_Global(.options = options).
-ControllerEventEn(false) :- Unit(), not nb.NB_Global().
-
-Flow(.logical_datapath = sw.dpname,
-     .stage            = switch_stage(IN, PRE_LB),
-     .priority         = 120,
-     .__match          = ip_dst_match ++ port_match,
-     .actions          = "trigger_event(event = \"empty_lb_backends\", "
-                         "vip = \"${k}\", protocol = \"${protocol}\", "
-                         "load_balancer = \"${lb._uuid}\");",
-     .external_ids     = stage_hint(lb._uuid)) :-
-    SwitchLBVIP(.sw = &sw, .lb = lb, .vip = vip),
-    (var k, var v) = vip,
-    v == "",
-    Some{var protocol} = set_nth(lb.protocol, 0),
-    Some{(var ip_address, var port, var addr_family)} = ip_address_and_port_from_lb_key(k),
-    var ip_dst_match = make_ip_dst_match(ip_address, addr_family) ++ " && ${protocol}",
-    var port_match = if (port != 0) { " && ${protocol} == ${port}" } else { "" }.
 
 for (SwitchHasLBVIP(&sw)) {
     Flow(.logical_datapath = sw.dpname,
@@ -4531,6 +4513,7 @@ for (RouterLBVIP(
      if is_some(l3dgw_port) or is_gateway)
 {
     if (vip_value == "") {
+        for (ControllerEventEn(true)) {
             for (HasEventElbMeter(has_elb_meter)) {
                 Some {(var __match, var __action)} =
                     build_empty_lb_event_flow(vip_key, lb, has_elb_meter) in
@@ -4541,6 +4524,7 @@ for (RouterLBVIP(
                      .actions          = __action,
                      .external_ids     = map_empty())
             }
+        }
     };
 
     /* A set to hold all ips that need defragmentation and tracking. */

commit 31f9b85cd336987b63d4716336bd1439edf117d1

Fixes: 90ab4b5af497 ("northd: Fix up controller event support.")

northd/ovn_northd.dl
@@ -1616,7 +1616,7 @@ function build_empty_lb_event_flow(key: string, lb: nb.Load_Balancer,
 /* ControllerEventEn has exactly one row, either 'true' to enable controller
  * events or 'false' to disable them. */
 relation ControllerEventEn(enable: bool)
-ControllerEventEn(map_get_bool_def(options, "controller-event", false)) :-
+ControllerEventEn(map_get_bool_def(options, "controller_event", false)) :-
     nb.NB_Global(.options = options).
 ControllerEventEn(false) :- Unit(), not nb.NB_Global().
 

northd/sb2ddlog
@@ -19,7 +19,6 @@ ovsdb2ddlog -f "$1" \
     -o RBAC_Role \
     -o RBAC_Permission \
     -o IP_Multicast \
-    -o Controller_Event \
     -o Service_Monitor \
     -p Datapath_Binding \
     -p Port_Binding \

tests/ovn-e2e.at
@@ -13837,16 +13837,21 @@ ovn-nbctl --wait=hv meter-add event-elb drop 100 pktps 10
 
 OVN_POPULATE_ARP
 ovn-nbctl --timeout=3 --wait=hv sync
-ovn-sbctl lflow-list
-as hv1 ovs-ofctl dump-flows br-int
+ovn-sbctl lflow-list > sbflows
+AT_CAPTURE_FILE([sbflows])
+as hv1 ovs-ofctl dump-flows br-int > offlows
+AT_CAPTURE_FILE([offlows])
 
 packet0="inport==\"sw0-p11\" && eth.src==00:00:00:00:00:11 && eth.dst==00:00:00:00:00:21 &&
          ip4 && ip.ttl==64 && ip4.src==192.168.1.11 && ip4.dst==192.168.1.100 &&
          tcp && tcp.src==10000 && tcp.dst==80"
 as hv1 ovs-appctl -t ovn-controller inject-pkt "$packet0"
 
-ovn-sbctl list controller_event
-uuid=$(ovn-sbctl list controller_event | awk '/_uuid/{print $3}')
+ovn-sbctl list controller_event > events
+AT_CAPTURE_FILE([events])
+uuid=$(awk '/_uuid/{print $3}' < events)
+echo uuid=$uuid
+AT_CHECK([test -n "$uuid"])
 AT_CHECK([ovn-sbctl get controller_event $uuid event_type], [0], [dnl
 empty_lb_backends
 ])